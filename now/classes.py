ea24bcd5bc0915e1d78ebb21b02c19ea141f374a
Luke Deen Taylor <luke@deentaylor.com>
2016-10-01 23:22:20 -0400

    Add __repr__ to Deployment class
    Allows deployments to look nicer in the console

--- now/classes.py
+++ now/classes.py
@@ -8,3 +8,9 @@ class Deployment:
         self.id = self.uid = data["uid"]
         self.url = data["url"]
         self.created = datetime.fromtimestamp(int(data["created"]) / 1000)
+    
+    def __repr__(self):
+        return "<Deployment '{}' on {}>".format(
+            self.name,
+            self.created.strftime("%b %d")
+        )
